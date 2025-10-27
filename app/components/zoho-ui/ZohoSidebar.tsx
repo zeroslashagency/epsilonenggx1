6e1535ffccda95c12bb8f7c294beb3e03dd7defd
Mubarak A <mubarakx01@gmail.com>
2025-10-28 03:29:24 +0530

    refactor: Change add users URL from /settings/users/add to /settings/add-users

--- app/components/zoho-ui/ZohoSidebar.tsx
+++ app/components/zoho-ui/ZohoSidebar.tsx
@@ -150,7 +150,7 @@ export function ZohoSidebar({ collapsed, onToggleAction }: ZohoSidebarProps) {
         {
           id: 'add-users',
           label: 'Add Users',
-          href: '/settings/users/add',
+          href: '/settings/add-users',
           icon: UserPlus
         },
         {
